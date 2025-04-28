bf220244d48b1d82f913e14296ad8ccbbe0c5d96
Cyprien023 <cyprienluzuy@gmail.com>
2025-04-28 10:35:18 +0200

    add bdd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 5000;
 
-app.use(cors());
+const projectsRoutes = require('./routes/projects');
 
-const projectRoutes = require('./src/routes/projects');
-app.use('/api/projects', projectRoutes);
+app.use('/api/projects', projectsRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
